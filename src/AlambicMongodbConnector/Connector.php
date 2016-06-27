8c879db1891b7f958687a0494ec7e44259bdc385
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-06-27 15:34:09 +0200

    WIP CRUD

--- src/AlambicMongodbConnector/Connector.php
+++ src/AlambicMongodbConnector/Connector.php
@@ -35,21 +35,22 @@ class Connector
     public function resolve($payload=[],$collection){
         $multivalued=isset($payload["multivalued"]) ? $payload["multivalued"] : false;
         $args=isset($payload["args"]) ? $payload["args"] : [];
-        if(!empty($args['_id'])){
-            $args['_id']=new \MongoId($args['_id']);
+        if(!empty($args['id'])){
+            $args['_id']=new \MongoId($args['id']);
+            unset($args['id']);
         }
         $result=[];
         if(!$multivalued){
             $result=$collection->findOne($args);
             if(!empty($result['_id'])){
-                $result['_id']=(string) $result['_id'];
+                $result['id']=(string) $result['_id'];
             }
         } else {
             $cursor = $collection->find($args);
             while ($cursor->hasNext()) {
                 $item=$cursor->getNext();
                 if(!empty($item['_id'])){
-                    $item['_id']=(string) $item['_id'];
+                    $item['id']=(string) $item['_id'];
                 }
                 $result[]=$item;
             }
@@ -59,8 +60,36 @@ class Connector
     }
 
     public function execute($payload=[],$collection){
-        //WIP
-        $payload["response"]=[];
+        $methodName=isset($payload["methodName"]) ? $payload["methodName"] : null;
+        if(empty($methodName)){
+            throw new Exception('MongoDB connector requires a valid methodName for write ops');
+        }
+        if(empty($args['id'])&&$methodName!='creation'){
+            throw new Exception('MongoDB connector requires id for operations other than create');
+        }
+        $result=[];
+        if(!empty($args['id'])){
+            $args['_id']=new \MongoId($args['id']);
+            unset($args['id']);
+        }
+        if($methodName=="create"){
+            $result=$collection->insert($args);
+            if(isset($result['_id'])){
+                $result['id'] = (string)$result['_id'];
+                unset($result['_id']);
+            }
+        } elseif ($methodName == "delete") {
+            $this->$collection->remove($args);
+        } elseif ($methodName == "update") {
+            $id= $args['_id'];
+            unset($args['_id']);
+            $result=$collection->findAndModify(["_id"=>$id],$args,null,["new"=>true]);
+            if(isset($result['_id'])){
+                $result['id'] = (string)$result['_id'];
+                unset($result['_id']);
+            }
+        }
+        $payload["response"]=$result;
         return $payload;
     }
 }
